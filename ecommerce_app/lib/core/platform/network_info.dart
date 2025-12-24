7fca705f46a93ffabda92d4051b14cfa2d495e40
samiyonas <samuel.x.yonas@gmail.com>
2025-12-24 14:05:36 +0300

    added network info implementation and test

--- ecommerce_app/lib/core/platform/network_info.dart
+++ ecommerce_app/lib/core/platform/network_info.dart
@@ -1,3 +1,14 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 abstract class NetWorkInfo {
   Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetWorkInfo {
+  final InternetConnectionChecker internetConnectionChecker;
+
+  NetworkInfoImpl({required this.internetConnectionChecker});
+
+  @override
+  Future<bool> get isConnected => internetConnectionChecker.hasConnection;
 }
---
+++ ecommerce_app/test/core/platform/network_info_test.dart
@@ -0,0 +1,34 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:ecommerce_app/core/platform/network_info.dart';
+
+class MockInternetConnectionChecker extends Mock
+    implements InternetConnectionChecker {}
+
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockInternetConnectionChecker mockInternetConnectionChecker;
+
+  setUp(() {
+    mockInternetConnectionChecker = MockInternetConnectionChecker();
+    networkInfo = NetworkInfoImpl(
+      internetConnectionChecker: mockInternetConnectionChecker,
+    );
+  });
+
+  group('isConnected', () {
+    test('should forward the call to InternetConnectionChecker.hasConnection',
+        () async {
+      // arrange
+      final tHasConnectionFuture = Future.value(true);
+      when(mockInternetConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+      // act
+      final result = networkInfo.isConnected;
+      // assert
+      verify(mockInternetConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
